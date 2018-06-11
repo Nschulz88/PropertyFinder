5043af1fc6093a6d72c0a0495161cf7fbbb1adb6
Nat Schulz <natalie.schulz@me.com>
2018-06-11 10:12:45 -0700

    basic homescreen set up

---
+++ App.js
@@ -0,0 +1,40 @@
+'use strict';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  NavigatorIOS
+} from 'react-native';
+import SearchPage from './SearchPage';
+
+type Props = {};
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Property Finder',
+          component: SearchPage,
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+    marginTop: 65,
+  },
+  container: {
+    flex: 1
+  }
+});
