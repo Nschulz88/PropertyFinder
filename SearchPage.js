2d595a2d4a5557ca4637ee05708b32fc5f745773
Nat Schulz <natalie.schulz@me.com>
2018-06-11 11:18:30 -0700

    text input & search button

--- SearchPage.js
+++ SearchPage.js
@@ -12,7 +12,36 @@ import {
 } from 'react-native';
 
 export default class SearchPage extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: 'london',
+      isLoading: false
+    };
+  }
+
+  _onSearchTextChange = (event) => {
+    console.log('_onSearchTextChange');
+    this.setState({
+      searchString: event.nativeEvent.text });
+    console.log('Current: ', this.state.searchString, ', Next: ', event.nativeEvent.text);
+  }
+
+  _executeQuery = (query) => {
+    console.log(query);
+    this.setState({
+      isLoading: true
+    })
+  }
+
+  _onSearchPressed = () => {
+    const query = urlForQueryAndPage('place-name', this.state.searchString, 1);
+    this._executeQuery(query);
+  }
+
   render() {
+    const spinner = this.state.isLoading ? 
+      <ActivityIndicator size='large'/> : null;
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
@@ -21,6 +50,20 @@ export default class SearchPage extends Component<{}> {
         <Text style={styles.description}>
           Search by place-name or postcode. 
         </Text>
+        <View style={styles.flowRight}>
+          <TextInput 
+            style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this._onSearchTextChange}
+            placeholder='Search via name or postcode'/>
+          <Button 
+            onPress={this._onSearchPressed}
+            color='#48BBEC'
+            title='Go'
+          />
+        </View>
+        <Image source={require('./Resources/house.png')} style={styles.images}/>
+        {spinner}
       </View>
     )
   }
@@ -37,5 +80,25 @@ const styles = StyleSheet.create({
     padding: 30,
     marginTop: 65,
     alignItems: 'center'
+  }, 
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  searchInput: {
+    height: 34, 
+    padding: 4, 
+    marginRight: 5,
+    flexGrow: 1, 
+    fontSize: 18, 
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC'
+  },
+  image: {
+    width: 217,
+    height: 138
   }
 })
